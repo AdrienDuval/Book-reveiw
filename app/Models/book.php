ed95b0b72082fd007719628581691a77f1c70835
adrien <annickalan67@gmail.com>
2023-08-05 16:35:02 +0530

    models updated

--- app/Models/book.php
+++ app/Models/book.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class book extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
