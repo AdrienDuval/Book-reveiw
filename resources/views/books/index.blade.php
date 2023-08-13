f94872a55629064197f8325e6501fd8d10f1f52c
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-08-13 15:24:32 +0530

    Update index.blade.php

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,12 +1,31 @@
 @extends('Layouts.app')
 
 @section('content')
-    <h1 class="mb-10 text-2xl font-bold text-gray-900 dark:text-white">Book</h1>
+    <h1 class="mb-10 text-2xl font-bold text-gray-900 dark:text-white">Books</h1>
     <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
-        <input type="text" class="input h-10" name="title" placeholder="Search by book title" value="{{ request('title') }}">
+        <input type="text" class="input h-10" name="title" placeholder="Search by book title"
+            value="{{ request('title') }}">
+            <input type="hidden" name="filter" value="{{ request('filter') }}">
         <button class="btn h-10">Search</button>
         <a href="{{ route('books.index') }}" class="btn btn-ghost h-10">Clear</a>
     </form>
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'latest',
+                'Popular_last_month' => 'popular Last Month',
+                'Popular_last_6months' => 'popular Last 6 Months',
+                'Highest_rated_last_month' => 'Highest Rated Last Month',
+                'Highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+        <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}" class="{{ request('filter') === $key || (request('filter') === null && $key === '')  ? 'filter-item-active' : 'filter-item' }}">
+            {{ $label }}
+        </a>
+        @endforeach
+    </div>
     <ul>
         @forelse ($books as $book)
             <li class="mb-4">
