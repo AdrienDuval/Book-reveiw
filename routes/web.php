e2c2d78346cdc50e434ce5dd13f52481b2129714
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-08-13 10:15:39 +0530

    Displaying the Book List

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('Book', BookController::class);
+
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
